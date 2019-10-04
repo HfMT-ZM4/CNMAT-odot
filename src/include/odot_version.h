cef9c316e3c25f7a7bbe4f00c3a83e80fe6ed3c3
maccallum <john.m@ccallum.com>
2019-10-04 15:11:02 +0200

    added print information to identify builds in this fork as HfMT

--- src/include/odot_version.h
+++ src/include/odot_version.h
@@ -11,19 +11,27 @@
 #include "report_compiler.h"
 //#define ODOT_VERSION "1.2.11" 
 //#define ODOT_RELEASE_DATE "[2012-12-21 Fri]"
-#define ODOT_COPYRIGHT_YEARS "2008-17"
-#define ODOT_AUTHORS "John MacCallum, Adrian Freed, Rama Gottfried, Ilya Rostovtsev"
-#define ODOT_COPYRIGHT_STRING "Regents of the University of California. All rights reserved."
+#define ODOT_COPYRIGHT_YEARS "2008-19"
+#define ODOT_AUTHORS "John MacCallum, Adrian Freed, Rama Gottfried, \
+Ilya Rostovtsev"
+#define ODOT_COPYRIGHT_STRING "Portions copyright Regents of the \
+University of California and Hochschule für Music und Theater, Hamburg. \
+All rights reserved."
+#define ODOT_NOTE "***Features in the HfMT-ZM4 build are \
+experimental and unsupported***"
 
 void __odot_version(void)
 {
 	post("odot version %s (compiled %s)", ODOT_VERSION, ODOT_COMPILE_DATE);
-    post("by %s\n", ODOT_AUTHORS);
+	post("by %s\n", ODOT_AUTHORS);
 
 #ifdef OMAX_PD_VERSION
-    post("alpha pd version");
+	post("alpha pd version");
 #endif
-	post("Copyright (c) %s %s", ODOT_COPYRIGHT_YEARS, ODOT_COPYRIGHT_STRING);
+	post("Copyright (c) %s %s\n%s",
+	     ODOT_COPYRIGHT_YEARS,
+	     ODOT_COPYRIGHT_STRING,
+	     ODOT_NOTE);
 }
 
 void odot_version(void *x)
@@ -32,7 +40,8 @@ void odot_version(void *x)
 		// called from main()--post only once
 		if(!(gensym("odot_version_has_been_posted")->s_thing)){
 			__odot_version();
-			gensym("odot_version_has_been_posted")->s_thing = (t_object *)1;
+			gensym("odot_version_has_been_posted")->s_thing =
+				(t_object *)1;
 		}
 	}else{
 		// called in response to the version message
@@ -41,9 +50,13 @@ void odot_version(void *x)
 	}
 #ifdef ODOT_DEPRECATED
 	if(x){
-		object_error((t_object *)x, "warning: %s is no longer supported and may be removed from the release at any time", OMAX_DOC_NAME);
+		object_error((t_object *)x,
+			     "warning: %s is no longer supported and may be \
+removed from the release at any time",
+			     OMAX_DOC_NAME);
 	}else{
-		error("warning: %s is no longer supported and may be removed from the release at any time", OMAX_DOC_NAME);
+		error("warning: %s is no longer supported and may be removed \
+from the release at any time", OMAX_DOC_NAME);
 	}
 #endif
 }
